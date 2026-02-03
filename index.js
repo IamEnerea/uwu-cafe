226edd60478850269d6644d8a45eff6632a0e30d
IamEnerea <enerea512@gmail.com>
2026-02-03 02:23:07 -0500

    normativa oficial uwu café

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ client.once(Events.ClientReady, async () => {
     .setTitle("📜 NORMATIVA — Uwu Café ☕🎀")
     .setColor(0xF6A5C0)
     .setDescription(
+      "ㅤ\n" +
       "✨ **Por favor, lee atentamente esta normativa antes de entrar en servicio.**\n" +
       "El cumplimiento de estas normas es obligatorio para todo el personal.\n\n" +
 
