57873d1a2d13e6990ae3d5e7aa9a69e146b10917
IamEnerea <enerea512@gmail.com>
2026-02-05 16:44:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const {
 } = require("discord.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages
+  ]
 });
 
 // ================= CONFIG =================
